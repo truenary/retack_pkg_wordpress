b689f1f272bc6e570b8521c51813f567239c569e
asheshbh11 <136300236+asheshbh11@users.noreply.github.com>
2024-08-15 13:39:10 +0545

    refac: minor changes

--- views/settings_page_content.php
+++ views/settings_page_content.php
@@ -3,7 +3,7 @@
         <img src="https://docs.retack.ai/wp-content/uploads/2024/08/retack_logo.png" alt="Retack AI">
     </div>
     <div class="banner">
-        <img src="https://docs.retack.ai/wp-content/uploads/2024/08/retack_main_with_bar.png" alt="Retack Dashboard">
+        <img src="https://docs.retack.ai/wp-content/uploads/2024/08/Frame-1000003629.png" alt="Retack Dashboard">
     </div>
     <div class="input-features">
         <div id="input">
@@ -42,12 +42,32 @@
                 <p>Features</p>
             </div>
 			<div class='border'></div>
-	            <ul class='features-list'>
-                <li>🔔 Get Instant Error Alerts</li>
-                <li>💡 Smart Insights</li>
-                <li>🛠️ Code Fix Automation</li>
-                <li>🎁 Generous Free Plan</li>
-            </ul>
+	            <div class='features-list'>
+				<div class="list-div">
+					<div class="alert">
+						<img src="https://docs.retack.ai/wp-content/uploads/2024/08/alert-2-1.png" alt="Error">
+					</div>
+					<p>Get Instant Error Alerts</p>
+				</div>
+                <div class="list-div">
+                    <div class="insight">
+                        <img src="https://docs.retack.ai/wp-content/uploads/2024/08/search-2-1.png" alt="Insights">
+                    </div>
+                    <p>Smart Insights</p>
+                </div>
+                <div class="list-div">
+                    <div class="fix">
+                        <img src="https://docs.retack.ai/wp-content/uploads/2024/08/process-1.png" alt="Code Fix">
+                    </div>
+                    <p>Code Fix Automation</p>
+                </div>
+                <div class="list-div">
+                    <div class='plan'>
+                        <img src="https://docs.retack.ai/wp-content/uploads/2024/08/gift-1.png" alt="Plan">
+                    </div>
+                    <p>Generous Free Plan</p>
+                </div>
+            </div>
             <div class="input-title-3">
                 <p>Effortless Integration with</p>
             </div>
